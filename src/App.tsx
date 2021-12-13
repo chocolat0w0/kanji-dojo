4a4fb78a1bbc55d43d1d6c6123d44771b09cc629
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2021-12-13 22:17:24 +0900

    漢字リストを学年別に表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,100 @@
 import React, { VFC } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+type Onyomi = {
+  yomi: string;
+  okuri?: string;
+};
+type Kanji = {
+  id: string;
+  ji: string;
+  kun: string[];
+  on: Onyomi[];
+  grade: number;
+};
+// TODO: スクレイピング候補 https://www.benricho.org/kanji/kyoikukanji/1nen.html
+// TODO: 別ファイルへ
+const kanjis: Kanji[] = [
+  {
+    id: '1',
+    ji: '一',
+    kun: ['イチ', 'イツ'],
+    on: [
+      {
+        yomi: 'ひと',
+      },
+      {
+        yomi: 'ひと',
+        okuri: 'つ',
+      },
+    ],
+    grade: 1,
+  },
+  {
+    id: '2',
+    ji: '二',
+    kun: ['二'],
+    on: [
+      {
+        yomi: 'ふた',
+      },
+      {
+        yomi: 'ふた',
+        okuri: 'つ',
+      },
+    ],
+    grade: 1,
+  },
+  {
+    id: '3',
+    ji: '伝',
+    kun: ['デン'],
+    on: [
+      {
+        yomi: 'つた',
+        okuri: 'わる',
+      },
+      {
+        yomi: 'つた',
+        okuri: 'える',
+      },
+      {
+        yomi: 'つた',
+        okuri: 'う',
+      },
+    ],
+    grade: 4,
+  },
+];
 
 const App: VFC = () => (
   <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload. test
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <header className="App-header">漢字道場</header>
+    <main>
+      {/* 漢字を学習年別に表示 */}
+      {[...Array(6).keys()]
+        .map((i) => i + 1)
+        .map((grade) => (
+          <section>
+            <h1>{grade}年の漢字</h1>
+            {kanjis
+              .filter((k) => k.grade === grade)
+              .map((k) => {
+                const { id } = k;
+
+                return (
+                  <label htmlFor={id}>
+                    <input id={id} name="kanji" type="checkbox" />
+                    {k.ji}
+                  </label>
+                );
+              })}
+          </section>
+        ))}
+      {/* 学習対象にした漢字を表示 */}
+
+      {/* 学習対象の漢字の問題文を表示 */}
+    </main>
   </div>
 );
 
