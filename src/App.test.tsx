79e31e8dd3b4ce70f36856b9f3f3c7a1da60dad4
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2021-12-27 23:33:55 +0900

    モックにあわせて漢字リストのテスト追加

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('初期表示では問題文に何も表示されない', async () => {
+test('初期表示：漢字リストが表示され、問題文に何も表示されない', async () => {
   render(<App />);
 
   // await の場合は get〜 ではなく find〜
   // https://testing-library.com/docs/dom-testing-library/api-async/
-  const examItem = await screen.findByText('問題文');
+  const kanjiItem = await screen.findByText('漢字リスト');
+  expect(kanjiItem.closest('section')?.querySelectorAll('input').length).toBe(
+    7,
+  );
+  expect(screen.getByLabelText('一')).toBeInTheDocument();
+  const examItem = screen.getByText('問題文');
   expect(examItem.closest('section')?.querySelectorAll('li').length).toBe(0);
 });
 
