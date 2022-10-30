d2fae3e61c408fbaea6dd66d4db4e721131a0c51
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2022-10-30 14:17:28 +0900

    ページルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,40 @@
-import React, { useCallback, useEffect, useState, VFC } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, VFC } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Select from 'components/pages/Select';
 import Count from 'components/pages/Count';
 import { SelectedKanjiType, SelectedStatusType } from 'data/SelectedKanjiType';
 import Question from 'components/pages/Question';
 
-type Mode = 'select' | 'count' | 'question' | 'answer';
-
 const App: VFC = () => {
-  const [mode, setMode] = useState<Mode>('select');
   const [statusList, setStatusList] = useState<SelectedStatusType[]>([]);
 
   const [selectedKanjiList, setSelectedKanjiList] = useState<
     SelectedKanjiType[]
   >([]);
 
-  // カウントモードは仮実装で、?count=1 クエリを URL に追加したらなる
-  const location = useLocation();
-  useEffect(() => {
-    if (mode === 'select' && location.search.includes('count=1'))
-      setMode('count');
-  }, [location, mode]);
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Select
+            statusList={statusList}
+            setStatusList={setStatusList}
+            setSelectedKanjiList={setSelectedKanjiList}
+          />
+        }
+      />
+      <Route path="count" element={<Count />} />
 
-  const changeNextMode = useCallback(() => {
-    if (mode === 'select') {
-      setMode('question');
-    }
-  }, [mode]);
-  const changePrevMode = useCallback(() => {
-    if (mode === 'question') {
-      setMode('select');
-    }
-  }, [mode]);
+      <Route
+        path="question"
+        element={<Question selectedKanjiList={selectedKanjiList} />}
+      />
 
-  const getComponent = useCallback(
-    (m: Mode): JSX.Element => {
-      switch (m) {
-        case 'select':
-          return (
-            <Select
-              statusList={statusList}
-              setStatusList={setStatusList}
-              setSelectedKanjiList={setSelectedKanjiList}
-              changeNextMode={changeNextMode}
-            />
-          );
-
-        case 'count':
-          return <Count />;
-
-        case 'question':
-          return (
-            <Question
-              selectedKanjiList={selectedKanjiList}
-              changePrevMode={changePrevMode}
-            />
-          );
-
-        default:
-          return <p>エラー</p>;
-      }
-    },
-    [changeNextMode, changePrevMode, selectedKanjiList, statusList],
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
-
-  return <div className="App">{getComponent(mode)}</div>;
 };
 
 export default App;
